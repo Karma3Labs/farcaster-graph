18e943547c8f79cd9edaecf37a57a872d9886350
Vijay Mariadassou <vijay@karma3labs.com>
2024-01-25 10:59:05 -0800

    first version of building farcaster graph

---
+++ pipeline/export_l1rep6rec3m12enhancedConnections.sql
@@ -0,0 +1,11 @@
+select 
+'0x' || encode(f1.custody_address::bytea, 'hex') as i,
+'0x' || encode(f2.custody_address::bytea, 'hex') as j,
+lt.v
+from 
+localtrust as lt
+inner join fids as f1 on (cast(lt.i as int8) = f1.fid)
+inner join fids as f2 on (cast(lt.j as int8) = f2.fid)
+where 
+strategy_id=3
+and date=(select max(date) from localtrust where strategy_id=3)
