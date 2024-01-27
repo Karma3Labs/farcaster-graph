4406d9c8ae354c699dede0ad0d2a4176d49146c1
Vijay Mariadassou <vijay@karma3labs.com>
2024-01-26 21:13:16 -0800

    code to export localtrust addresses instead of fids

---
+++ pipeline/export_existingConnections_addr.sql
@@ -0,0 +1,11 @@
+SELECT 
+	'0x'||encode(f1.custody_address,'hex') as i,
+ 	'0x'||encode(f2.custody_address,'hex') as j, 
+  lt.v
+FROM localtrust as lt
+INNER JOIN fids as f1 on (f1.fid = cast(lt.i as int8))
+INNER JOIN fids as f2 on (f2.fid = cast(lt.j as int8))
+WHERE 
+  lt.strategy_id=1
+  AND lt.date=(select max(date) from localtrust where strategy_id=1)
+
