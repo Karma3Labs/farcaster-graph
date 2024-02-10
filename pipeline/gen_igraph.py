d7d8d0705b0ab07b3245be74b88581785b8b68da
Vijay Mariadassou <vijay@karma3labs.com>
2024-02-10 09:17:48 -0800

    pickle idx file used to factorize i j addresses

--- pipeline/gen_igraph.py
+++ pipeline/gen_igraph.py
@@ -20,16 +20,20 @@ def main(incsv:Path,  outdir:Path, prefix:str, logger:logging.Logger):
   logger.info(utils.df_info_to_string(edges_df, with_sample=True))
   with Timer(name="factorize"):
     stacked = edges_df[['i','j']].stack()
-    edges_df[['i_code', 'j_code']] = pd.Series(stacked.factorize()[0], index=stacked.index).unstack()
+    codes, uniqs = stacked.factorize()
+    edges_df[['i_code', 'j_code']] = pd.Series(codes, index=stacked.index).unstack()
+    idx_df = pd.DataFrame(uniqs)
   logger.info(utils.df_info_to_string(edges_df, with_sample=True))
   with Timer(name="df_to_igraph"):
     g = ig.Graph.DataFrame(edges_df, directed=True, use_vids=False)
   logger.info(ig.summary(g))
   with Timer(name="write_pickle"):
     dfile = os.path.join(outdir, f"{prefix}_df.pkl")
+    ifile = os.path.join(outdir, f"{prefix}_idx.pkl")
     gfile = os.path.join(outdir, f"{prefix}_ig.pkl")
-    logger.info(f"Saving dataframe to {dfile}  and graph to {gfile}")
+    logger.info(f"Saving dataframe to {dfile} index to {ifile} and graph to {gfile}")
     edges_df.to_pickle(dfile)
+    idx_df.to_pickle(ifile)
     g.write_pickle(gfile)
 
 
