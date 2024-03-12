841f994dbdbb6788ba121bd35e4797baaec00634
Vijay Mariadassou <vijay@karma3labs.com>
2024-03-12 07:42:06 -0700

    added utility script for testing

---
+++ pipeline/globaltrust/test_data.py
@@ -0,0 +1,29 @@
+# standard dependencies
+import logging
+
+# local dependencies
+import utils
+from config import settings
+from . import db_utils
+from .queries import IJVSql
+
+# 3rd party dependencies
+from dotenv import load_dotenv
+import pandas as pd
+
+if __name__ == '__main__':
+  load_dotenv()
+  print(settings)
+
+  logger = logging.getLogger()
+  utils.setup_filelogger(logger, __file__)
+  logger.setLevel(logging.DEBUG)
+  utils.setup_consolelogger(logger)
+
+  pg_dsn = settings.POSTGRES_DSN.get_secret_value()
+
+  df = db_utils.ijv_df_read_sql_tmpfile(logger, pg_dsn, IJVSql.FOLLOWS)
+  logger.info(utils.df_info_to_string(df, with_sample=True))
+
+  num_ij_pairs = df.groupby(['i', 'j']).ngroups
+  logger.info(f"Unique i,j pairs: {num_ij_pairs}")
