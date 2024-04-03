541325577017a65f16cfb7bf8bc945247d0e5418
Vijay Mariadassou <vijay@karma3labs.com>
2024-04-03 15:29:29 -0700

    create db pool before loading graphs

--- serve/app/main.py
+++ serve/app/main.py
@@ -80,14 +80,14 @@ async def check_and_reload_models(loader: GraphLoader):
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Execute when API is started"""
+    app_state['db_pool'] = await asyncpg.create_pool(settings.POSTGRES_URI.get_secret_value(), 
+                                         min_size=1, 
+                                         max_size=settings.POSTGRES_POOL_SIZE)
     logger.warning(f"{settings}")
     app_state['graph_loader'] = GraphLoader()
     app_state['graph_loader_task'] = asyncio.create_task(
         check_and_reload_models(app_state['graph_loader'])
     )
-    app_state['db_pool'] = await asyncpg.create_pool(settings.POSTGRES_URI.get_secret_value(), 
-                                         min_size=1, 
-                                         max_size=settings.POSTGRES_POOL_SIZE)
     yield
     """Execute when API is shutdown"""
     await app_state['db_pool'].close()
