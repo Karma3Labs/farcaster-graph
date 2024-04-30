93edac868e97a70a95f83fab635b307896817a2b
Vijay Mariadassou <vijay@karma3labs.com>
2024-04-29 19:32:49 -0700

    feat: first implementation of pipeline for generating channel rankings

--- pipeline/channels/main.py
+++ pipeline/channels/main.py
@@ -1,5 +1,6 @@
 # standard dependencies
 import sys
+import os
 import argparse
 import asyncio
 import random
@@ -7,19 +8,20 @@ from pathlib import Path
 
 # local dependencies
 from config import settings
-from . import channel_model
 import utils
+from timer import Timer
+from . import channel_utils
 from . import go_eigentrust
 
 # 3rd party dependencies
 from dotenv import load_dotenv
 from loguru import logger
-import niquests as requests
 import pandas
 
 logger.remove()
 level_per_module = {
     "": settings.LOG_LEVEL,
+    "channels.channel_utils": "TRACE",
     "silentlib": False
 }
 logger.add(sys.stdout,
@@ -28,60 +30,13 @@ logger.add(sys.stdout,
            filter=level_per_module,
            level=0)
 
-def fetch_all_channels() -> list[channel_model.Channel]:
-  url = 'https://api.warpcast.com/v2/all-channels'
-  response = requests.get(url,headers = {
-                              'Accept': 'application/json',
-                              'Content-Type': 'application/json'
-                              },
-                          timeout=settings.WARPCAST_CHANNELS_TIMEOUT)
-  if response.status_code != 200:
-      logger.error(f"Server error: {response.status_code}:{response.reason}")
-      raise Exception(f"Server error: {response.status_code}:{response.reason}")
-  data = response.json()['result']['channels']
-  return [channel_model.Channel(c) for c in data]
 
-def fetch_channel(channel_id: str) -> channel_model.Channel:
-  url = f'https://api.warpcast.com/v1/channel?channelId={channel_id}'
-  logger.info(url)
-  response = requests.get(url,headers = {
-                              'Accept': 'application/json',
-                              'Content-Type': 'application/json'
-                              },
-                          timeout=settings.WARPCAST_CHANNELS_TIMEOUT)
-  if response.status_code != 200:
-      logger.error(f"Server error: {response.status_code}:{response.reason}")
-      raise Exception(f"Server error: {response.status_code}:{response.reason}")
-  data = response.json()['result']['channel']
-  return channel_model.Channel(data) 
-
-
-def fetch_channel_followers(channel_id: str) -> list[int]:
-  url = f'https://api.warpcast.com/v1/channel-followers?channelId={channel_id}'
-  logger.info(url)
-  fids = []
-
-  next_url = url
-  while True:
-    response = requests.get(next_url,headers = {
-                                'Accept': 'application/json',
-                                'Content-Type': 'application/json'
-                                },
-                            timeout=settings.WARPCAST_CHANNELS_TIMEOUT)
-    if response.status_code != 200:
-        logger.error(f"Server error: {response.status_code}:{response.reason}")
-        raise Exception(f"Server error: {response.status_code}:{response.reason}")
-    body = response.json()
-    fids.extend(body['result']['fids'])
-    if 'next' in body and 'cursor' in body['next'] and body['next']['cursor']:
-      cursor = body['next']['cursor']
-      next_url = f"{url}&cursor={cursor}"
-      logger.info(next_url)
-    else:
-      break
-  return fids
-
-async def main(localtrust_pkl: Path, channel_ids: list[str]):
+@Timer(name="main")
+async def main(
+  localtrust_pkl: Path, 
+  channel_ids: list[str],
+  outdir:Path
+):
   # load localtrust
   utils.log_memusage(logger)
   logger.info(f"loading {localtrust_pkl}")
@@ -89,35 +44,45 @@ async def main(localtrust_pkl: Path, channel_ids: list[str]):
   logger.info(utils.df_info_to_string(global_lt_df, with_sample=True))
   utils.log_memusage(logger)
 
+  all_channel_scores_lst = []
   # for each channel, take a slice of localtrust and run go-eigentrust
   for cid in channel_ids:
-    channel = fetch_channel(channel_id=cid)
-    fids = fetch_channel_followers(channel_id=cid)
+    channel = channel_utils.fetch_channel(channel_id=cid)
+    fids = channel_utils.fetch_channel_followers(channel_id=cid)
 
     logger.info(f"Channel details: {channel}")
     logger.info(f"Number of channel followers: {len(fids)}")
     logger.info(f"Sample of channel followers: {random.sample(fids, 5)}")
 
-    channel_lt_df = global_lt_df[global_lt_df['i'].isin(fids) & global_lt_df['j'].isin(fids)]
+    fids.extend(channel.host_fids) # host_fids need to be included in the eigentrust compute
 
-    pt_len = len(channel.host_fids)
-    pretrust = [{'i': id, 'v': 1/pt_len} for id in channel.host_fids]
-    max_pt_id = max(channel.host_fids)
+    with Timer(name="channel_localtrust"):
+      # TODO Perf - fids should be a 'set' and not a 'list'
+      # TODO Perf - filter using 'query' instead of 'isin'
+      channel_lt_df = global_lt_df[global_lt_df['i'].isin(fids) & global_lt_df['j'].isin(fids)]
+      logger.info(utils.df_info_to_string(channel_lt_df, with_sample=True))
     
-    localtrust = channel_lt_df.to_dict(orient="records")
-    max_lt_id = max(channel_lt_df['i'].max(), channel_lt_df['j'].max())
+    if len(channel_lt_df) == 0:
+      logger.error(f"No localtrust for channel {cid}")
+      continue
 
-    globaltrust = go_eigentrust.go_eigentrust(logger, 
-                                              pretrust,
-                                              max_pt_id,
-                                              localtrust,
-                                              max_lt_id
-                                              )
-    logger.info(f"go_eigentrust returned {len(globaltrust)} entries")
+    with Timer(name="go_eigntrust"):
+      scores = go_eigentrust.get_scores(lt_df=channel_lt_df, pt_ids=channel.host_fids)
+    logger.info(f"go_eigentrust returned {len(scores)} entries")
+    logger.debug(f"channel user scores:{scores}")
+    scores_df = pandas.DataFrame(data=scores)
+    scores_df['channel_id'] = cid
+    all_channel_scores_lst.append(scores_df)
     utils.log_memusage(logger)
+  # end of for loop 
 
-    # rename i and v to fid and score respectively
-    fid_scores = [ {'fid': score['i'], 'score': score['v']} for score in globaltrust]
+  all_df = pandas.concat(all_channel_scores_lst)
+  logger.info(utils.df_info_to_string(all_df, with_sample=True))
+  utils.log_memusage(logger)
+  outfile = os.path.join(outdir, "channel_ranking_df.pkl")
+  logger.info(f"Pickling channel rankings to {outfile}")
+  all_df.to_pickle(outfile)
+
 
 # How to run this program:
 # cd farcaster-graph/pipeline
@@ -133,6 +98,10 @@ if __name__ == "__main__":
                     help="input localtrust pkl file",
                     required=True,
                     type=lambda f: Path(f).expanduser().resolve())  
+  parser.add_argument("-o", "--outdir",
+                    help="output directory for channel ranking pickle file",
+                    required=True,
+                    type=lambda f: Path(f).expanduser().resolve())
 
   args = parser.parse_args()
   print(args)
@@ -141,4 +110,4 @@ if __name__ == "__main__":
   print(settings)
 
   logger.debug('hello main')
-  # asyncio.run(main(localtrust_pkl=args.localtrust, channel_ids=args.ids))
+  asyncio.run(main(localtrust_pkl=args.localtrust, channel_ids=args.ids, outdir=args.outdir))
--- pipeline/run_channel_scraper.sh
+++ pipeline/run_channel_scraper.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-while getopts v: flag
+while getopts w:i:o:v: flag
 do
     case "${flag}" in
         w) WORK_DIR=${OPTARG};;
+        i) IN_LT=${OPTARG};;
+        o) OUT_DIR=${OPTARG};;
         v) VENV=${OPTARG};;
     esac
 done
 
 if [ -z "$VENV" ]; then
-  echo "Usage:   $0 -v [venv]"
+  echo "Usage:   $0 -w [work_dir] -i [in_lt] -o [out_dir] -v [venv]"
   echo ""
-  echo "Example: $0 -v /home/ubuntu/venvs/fc-graph-env3/"
+  echo "Example: $0 -w . -i /home/ubuntu/serve_files/fc_engagement_fid_df.pkl -o /tmp -v /home/ubuntu/venvs/fc-graph-env3/"
   echo ""
   echo "Params:"
+  echo "  [work_dir]  The working directory to read .env file and execute scripts from."
+  echo "  [in_lt] The input localtrust file that the channel rankings is based on."
+  echo "  [out_dir] The output directory to write the channel ranking file."
   echo "  [venv] The path where a python3 virtualenv has been created."
   echo ""
   exit
 fi
+
+source $WORK_DIR/.env
 
 # set -x
 set -e
@@ -27,7 +34,9 @@ function log() {
   echo "`date` - $1"
 }
 
+mkdir -p $OUT_DIR
+
 source $VENV/bin/activate
 pip install -r requirements.txt
-python3 -m channels.main
+python3 -m channels.main -i farcaster degen base optimism founders -l $IN_LT -o $OUT_DIR 
 deactivate
