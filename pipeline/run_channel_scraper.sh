ffac1af0699a018a96d31e79fcb0b43a73bd15fc
Vijay Mariadassou <vijay@karma3labs.com>
2024-05-28 09:32:14 -0700

    remove localtrust dependency for channel ranking

--- pipeline/run_channel_scraper.sh
+++ pipeline/run_channel_scraper.sh
@@ -4,19 +4,17 @@ while getopts w:i:v: flag
 do
     case "${flag}" in
         w) WORK_DIR=${OPTARG};;
-        i) IN_LT=${OPTARG};;
         v) VENV=${OPTARG};;
     esac
 done
 
 if [ -z "$VENV" ]; then
-  echo "Usage:   $0 -w [work_dir] -i [in_lt] -v [venv]"
+  echo "Usage:   $0 -w [work_dir] -v [venv]"
   echo ""
-  echo "Example: $0 -w . -i /home/ubuntu/serve_files/fc_engagement_fid_df.pkl -v /home/ubuntu/venvs/fc-graph-env3/"
+  echo "Example: $0 -w . -v /home/ubuntu/venvs/fc-graph-env3/"
   echo ""
   echo "Params:"
   echo "  [work_dir]  The working directory to read .env file and execute scripts from."
-  echo "  [in_lt] The input localtrust file that the channel rankings is based on."
   echo "  [venv] The path where a python3 virtualenv has been created."
   echo ""
   exit
@@ -34,5 +32,5 @@ function log() {
 
 source $VENV/bin/activate
 pip install -r requirements.txt
-python3 -m channels.main -i founders degen base optimism farcaster op-stack new-york -l $IN_LT
+python3 -m channels.main -i founders degen base optimism farcaster op-stack new-york 
 deactivate
