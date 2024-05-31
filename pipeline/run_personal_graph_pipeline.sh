e326f4880a248cf9c18c338acc3bc04d47b22fc4
Vijay Mariadassou <vijay@karma3labs.com>
2024-05-31 12:12:47 -0700

    backup of in progress files during pre-compute

--- pipeline/run_personal_graph_pipeline.sh
+++ pipeline/run_personal_graph_pipeline.sh
@@ -24,6 +24,7 @@ if [ -z "$IN_CSV" ] || [ -z "$OUT_DIR" ] || [ -z "$WORK_DIR" ] || [ -z "$VENV" ]
   exit
 fi
 
+JOBTIME=$(date +%Y%m%d%H%M%S)
 
 source $WORK_DIR/.env
 
@@ -31,8 +32,20 @@ source $WORK_DIR/.env
 set -e
 set -o pipefail
 
+mkdir -p ${OUT_DIR}/temp-${JOBTIME}
+
 source $VENV/bin/activate
 pip install -r requirements.txt
-python3 -m graph.gen_personal_graph_amp -i $IN_CSV -o $OUT_DIR -p 10 -c 100 -m 1000
-python3 -m graph.rechunk_graph_pqt -i $OUT_DIR -o $OUT_DIR/personal_graph.parquet
+
+# generate graph with 10 processes, 100 child threads and 1000 neighbors
+python3 -m graph.gen_personal_graph_amp -i $IN_CSV -o ${OUT_DIR}/temp-${JOBTIME} -p 10 -c 100 -m 1000
+
+# if graph creation succeeded, replace previous job output with current job output 
+rm -rf ${OUT_DIR}/temp
+mv ${OUT_DIR}/temp-${JOBTIME} ${OUT_DIR}/temp
+
+# consolidate approx 4000 small pqt files into 1 big parquet file
+python3 -m graph.rechunk_graph_pqt -i ${OUT_DIR}/temp -o $OUT_DIR/personal_graph.parquet
+
+# TODO copy personal_graph.parquet to S3
 deactivate
