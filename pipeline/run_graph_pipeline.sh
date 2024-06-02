0f949db93d99199641417580fe6cc59bd2180be2
Vijay Mariadassou <vijay@karma3labs.com>
2024-06-02 16:44:30 -0700

    fix: syntax bugs

--- pipeline/run_graph_pipeline.sh
+++ pipeline/run_graph_pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts w:o:s:v: flag
+while getopts o:s: flag
 do
     case "${flag}" in
         o) OUT_DIR=${OPTARG};;
@@ -11,7 +11,7 @@ done
 if [ -z "$OUT_DIR" ] || [ -s "$S3_BKT" ]; then
   echo "Usage:   $0 -o [out_dir] -s [s3_bkt]"
   echo ""
-  echo "Example: $0 -o /tmp -s k3l-openrank-farcaster"
+  echo "Example: $0 -o /home/ubuntu/serve_files -s k3l-openrank-farcaster"
   echo ""
   echo "Params:"
   echo "  [out_dir]   The output directory to write the graph file."
@@ -20,21 +20,22 @@ if [ -z "$OUT_DIR" ] || [ -s "$S3_BKT" ]; then
   exit
 fi
 
-# set -x
+set -x
 set -e
 set -o pipefail
 
-aws s3 cp s3://${S3_BKT}/personal_graph.parquet $OUT_DIR/personal_graph.parquet.new
+# aws s3 cp s3://${S3_BKT}/personal_graph.parquet $OUT_DIR/personal_graph.parquet.new
+cp ../serve/samples/personal_graph.parquet $OUT_DIR/personal_graph.parquet.new
 
 if [ -n "$OUT_DIR/personal_graph.parquet" ]; then
-  new_size=`du -m $OUT_DIR/personal_graph.parquet.new | cut -f1`
-  prev_size=`du -m $OUT_DIR/personal_graph.parquet | cut -f1`
+  new_size=`du -k $OUT_DIR/personal_graph.parquet.new | cut -f1`
+  prev_size=`du -k $OUT_DIR/personal_graph.parquet | cut -f1`
   size_diff="$((new_size-prev_size))"
   if [[ $size_diff -lt -100 ]]; then
-    then
-      echo 'New graph smaller than previously generated graph. Abort script.'
-      exit 1
+    echo 'New graph smaller than previously generated graph. Abort script.'
+    exit 1
   fi
+fi
 mv $OUT_DIR/personal_graph.parquet.new $OUT_DIR/personal_graph.parquet
 # touch success file so that serving code can reload
 touch $OUT_DIR/personal_graph_SUCCESS
