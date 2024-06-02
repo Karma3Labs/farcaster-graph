97510c9dbc47d895fb15ffd0761e1939914a1506
Vijay Mariadassou <vijay@karma3labs.com>
2024-06-02 08:27:30 -0700

    copy personal-graph to S3; overwrite previous personal graph only if new graph runs successfully

--- pipeline/run_personal_graph_pipeline.sh
+++ pipeline/run_personal_graph_pipeline.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-while getopts i:o:w:v: flag
+while getopts i:o:s:w:v: flag
 do
     case "${flag}" in
         i) IN_CSV=${OPTARG};;
         o) OUT_DIR=${OPTARG};;
+        s) S3_BKT=${OPTARG};;
         w) WORK_DIR=${OPTARG};;
         v) VENV=${OPTARG};;
     esac
 done
 
-if [ -z "$IN_CSV" ] || [ -z "$OUT_DIR" ] || [ -z "$WORK_DIR" ] || [ -z "$VENV" ]; then
-  echo "Usage:   $0 -i [in_csv] -o [out_dir] -w [work_dir] -v [venv]"
+if [ -z "$IN_CSV" ] || [ -z "$WORK_DIR" ] || [ -z "$VENV" ] || [ -z "$OUT_DIR" ]|| [ -z "$S3_BKT" ]; then
+  echo "Usage:   $0 -i [in_csv] -w [work_dir] -v [venv] -o [out_dir] -s [s3_bkt] "
   echo ""
-  echo "Example: $0 -i /home/ubuntu/serve_files/lt_engagement_fid.csv -w . -o /tmp/personal-graph/ -v .venv"
+  echo "Example: $0 \ "
+  echo "  -i /home/ubuntu/serve_files/lt_engagement_fid.csv \ "
+  echo "  -w . \ "
+  echo "  -v .venv \ "
+  echo "  -o /tmp/personal-graph/ \ "
+  echo "  -s k3l-openrank-farcaster"
   echo ""
   echo "Params:"
   echo "  [in_csv]  The source file to read dataframe from."
@@ -24,11 +30,12 @@ if [ -z "$IN_CSV" ] || [ -z "$OUT_DIR" ] || [ -z "$WORK_DIR" ] || [ -z "$VENV" ]
   exit
 fi
 
+
 JOBTIME=$(date +%Y%m%d%H%M%S)
 
 source $WORK_DIR/.env
 
-# set -x
+set -x
 set -e
 set -o pipefail
 
@@ -38,14 +45,38 @@ source $VENV/bin/activate
 pip install -r requirements.txt
 
 # generate graph with 10 processes, 100 child threads and 1000 neighbors
-python3 -m graph.gen_personal_graph_amp -i $IN_CSV -o ${OUT_DIR}/temp-${JOBTIME} -p 10 -c 100 -m 1000
+python3 -m graph.gen_personal_graph_amp -i $IN_CSV -o ${OUT_DIR}/temp-${JOBTIME} -p 28 -c 100 -m 1000
+
+# if previous graph compute exists, confirm that the new output is larger in size
+if [ -d "$OUT_DIR/temp" ]; then
+  new_size=`du -m ${OUT_DIR}/temp-${JOBTIME} | cut -f1`
+  prev_size=`du -m ${OUT_DIR}/temp | cut -f1`
+  size_diff="$((new_size-prev_size))"
+  if [[ $size_diff -lt -100 ]]; then
+    echo 'New graph parts much smaller than previously generated graph parts. Abort script.'
+    exit 1
+  fi
+fi
 
 # if graph creation succeeded, replace previous job output with current job output 
 rm -rf ${OUT_DIR}/temp
 mv ${OUT_DIR}/temp-${JOBTIME} ${OUT_DIR}/temp
 
 # consolidate approx 4000 small pqt files into 1 big parquet file
-python3 -m graph.rechunk_graph_pqt -i ${OUT_DIR}/temp -o $OUT_DIR/personal_graph.parquet
+python3 -m graph.rechunk_graph_pqt -i ${OUT_DIR}/temp -o $OUT_DIR/personal_graph.parquet.new
 
-# TODO test size of parquet and push to S3
+# if previous graph file exists, confirm that the new file is larger in size
+if [ -n "$OUT_DIR/personal_graph.parquet" ]; then
+  new_size=`du -m $OUT_DIR/personal_graph.parquet.new | cut -f1`
+  prev_size=`du -m $OUT_DIR/personal_graph.parquet | cut -f1`
+  size_diff="$((new_size-prev_size))"
+  if [[ $size_diff -lt -100 ]]; then
+    echo 'New graph file much smaller than previously generated graph file. Abort script.'
+    exit 1
+  fi
+fi
+mv $OUT_DIR/personal_graph.parquet.new $OUT_DIR/personal_graph.parquet
+
 deactivate
+
+aws s3 cp $OUT_DIR/personal_graph.parquet s3://${S3_BKT}/
