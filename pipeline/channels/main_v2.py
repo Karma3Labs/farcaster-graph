99fed39ef60a8f278a027735adb0873cb1d48236
SeungJu Lee <sj@karma3labs.com>
2024-06-22 03:06:01 +0000

    feat: parallelize dag_gen_channel_ranking

---
+++ pipeline/channels/main_v2.py
@@ -0,0 +1,170 @@
+# standard dependencies
+import sys
+import argparse
+import random
+import pandas as pd
+
+# local dependencies
+import utils
+import db_utils
+import go_eigentrust
+from config import settings
+from timer import Timer
+from . import channel_utils
+from . import compute_trust
+
+# 3rd party dependencies
+from dotenv import load_dotenv
+from loguru import logger
+import niquests
+from urllib3.util import Retry
+
+# Performance optimization to avoid copies unless there is a write on shared data
+pd.set_option("mode.copy_on_write", True)
+
+# Configure logger
+logger.remove()
+level_per_module = {
+    "": settings.LOG_LEVEL,
+    "db_utils": "DEBUG",
+    "silentlib": False
+}
+logger.add(sys.stdout,
+           colorize=True,
+           format=settings.LOGURU_FORMAT,
+           filter=level_per_module,
+           level=0)
+
+load_dotenv()
+
+def fetch_channel_data(csv_path):
+    try:
+        channel_data = channel_utils.get_seed_fids_from_csv(csv_path)
+        channel_ids = channel_data["channel id"].values.tolist()
+        return channel_ids
+    except Exception as e:
+        logger.error(f"Failed to read channel data from CSV: {e}")
+        return []
+
+def process_channel(cid, channel_data, http_session, pg_dsn, pg_url):
+    try:
+        channel = channel_utils.fetch_channel(http_session=http_session, channel_id=cid)
+        host_fids = list(set(int(fid) for fid in channel_data[channel_data["channel id"] == cid]["seed_fids_list"].values[0]))
+    except Exception as e:
+        logger.error(f"Failed to fetch channel details for channel {cid}: {e}")
+        return {cid: []}
+
+    logger.info(f"Channel details: {channel}")
+
+    utils.log_memusage(logger)
+    try:
+        global_lt_df = compute_trust._fetch_interactions_df(logger, pg_dsn, channel_url=channel.project_url)
+        global_lt_df = global_lt_df.rename(columns={'l1rep6rec3m12': 'v'})
+        logger.info(utils.df_info_to_string(global_lt_df, with_sample=True))
+        utils.log_memusage(logger)
+    except Exception as e:
+        logger.error(f"Failed to fetch global interactions DataFrame: {e}")
+        return {cid: []}
+
+    try:
+        fids = db_utils.fetch_channel_participants(pg_dsn=pg_dsn, channel_url=channel.project_url)
+    except Exception as e:
+        logger.error(f"Failed to fetch channel participants for channel {cid}: {e}")
+        return {cid: []}
+
+    logger.info(f"Number of channel followers: {len(fids)}")
+    if len(fids) > 5:
+        logger.info(f"Sample of channel followers: {random.sample(fids, 5)}")
+    else:
+        logger.info(f"All channel followers: {fids}")
+
+    fids.extend(host_fids)  # host_fids need to be included in the eigentrust compute
+
+    try:
+        channel_lt_df = global_lt_df[global_lt_df['i'].isin(fids) & global_lt_df['j'].isin(fids)]
+        logger.info(utils.df_info_to_string(channel_lt_df, with_sample=True))
+    except Exception as e:
+        logger.error(f"Failed to compute local trust for channel {cid}: {e}")
+        return {cid: []}
+
+    present_fids = list(set(host_fids).intersection(channel_lt_df['i'].values))
+    absent_fids = list(set(host_fids) - set(channel_lt_df['i'].values))
+    logger.info(f"Absent Fids for the channel: {absent_fids}")
+
+    if len(channel_lt_df) == 0:
+        logger.error(f"No local trust for channel {cid}")
+        return {cid: absent_fids}
+
+    try:
+        scores = go_eigentrust.get_scores(lt_df=channel_lt_df, pt_ids=present_fids)
+    except Exception as e:
+        logger.error(f"Failed to compute EigenTrust scores for channel {cid}: {e}")
+        return {cid: absent_fids}
+
+    logger.info(f"go_eigentrust returned {len(scores)} entries")
+    logger.debug(f"Channel user scores: {scores}")
+
+    scores_df = pd.DataFrame(data=scores)
+    scores_df['channel_id'] = cid
+    scores_df.rename(columns={'i': 'fid', 'v': 'score'}, inplace=True)
+    scores_df['rank'] = scores_df['score'].rank(
+        ascending=False,
+        method='first'
+    ).astype(int)
+    scores_df['strategy_name'] = 'channel_engagement'
+    logger.info(utils.df_info_to_string(scores_df, with_sample=True))
+    utils.log_memusage(logger)
+
+    try:
+        logger.info(f"Inserting data into the database for channel {cid}")
+        db_utils.df_insert_copy(pg_url=pg_url, df=scores_df, dest_tablename=settings.DB_CHANNEL_FIDS)
+    except Exception as e:
+        logger.error(f"Failed to insert data into the database for channel {cid}: {e}")
+
+    return {cid: absent_fids}
+
+def process_channels(csv_path: str, channel_ids_str: str):
+    # Setup connection pool for querying Warpcast API
+    retries = Retry(
+        total=3,
+        backoff_factor=0.1,
+        status_forcelist=[502, 503, 504],
+        allowed_methods=['GET']
+    )
+    http_session = niquests.Session(retries=retries)
+
+    pg_dsn = settings.POSTGRES_DSN.get_secret_value()
+    pg_url = settings.POSTGRES_URL.get_secret_value()
+
+    channel_data = channel_utils.get_seed_fids_from_csv(csv_path)
+    channel_ids = channel_ids_str.split(',')
+    missing_seed_fids = []
+
+    for cid in channel_ids:
+        result = process_channel(cid, channel_data, http_session, pg_dsn, pg_url)
+        missing_seed_fids.append(result)
+
+    logger.info(missing_seed_fids)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--csv", type=str, help="path to the CSV file. For example, -c /path/to/file.csv", required=True)
+    parser.add_argument("-t", "--task", type=str, help="task to perform: fetch or process", required=True)
+    parser.add_argument("-ids", "--channel_ids", type=str, help="channel IDs for processing, only used for process task", required=False)
+
+    args = parser.parse_args()
+    print(args)
+
+    logger.debug('hello main')
+
+    if args.task == 'fetch':
+        channel_ids = fetch_channel_data(args.csv)
+        print(','.join(channel_ids))  # Print channel_ids as comma-separated for Airflow XCom
+    elif args.task == 'process':
+        if args.channel_ids:
+            process_channels(args.csv, args.channel_ids)
+        else:
+            logger.error("Channel IDs are required for processing.")
+            sys.exit(1)
+    else:
+        logger.error("Invalid task specified. Use 'fetch' or 'process'.")
---
+++ pipeline/dags/dag_gen_channel_ranking_v2.py
@@ -0,0 +1,62 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+import math
+
+default_args = {
+    'owner': 'coder2j',
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2)
+}
+
+PIPELINE_DIR = './'
+VENV_DIR = './.venv'
+SHELL_SCRIPT = f'{PIPELINE_DIR}/run_channel_scraper_v2.sh'
+CSV_PATH = f'{PIPELINE_DIR}/channels/Top_Channels.csv'
+N_CHUNKS = 10  # Define the number of chunks
+
+def extract_channel_ids(**kwargs):
+    ti = kwargs['ti']
+    channel_ids = ti.xcom_pull(task_ids='fetch_channel_data')
+    channel_ids_list = channel_ids.split(',')
+    print(f"Extracted channel IDs: {channel_ids_list}")
+    chunk_size = math.ceil(len(channel_ids_list) / N_CHUNKS)
+    channel_chunks = [channel_ids_list[i:i + chunk_size] for i in range(0, len(channel_ids_list), chunk_size)]
+    print(f"Channel chunks: {channel_chunks}")
+    for idx, chunk in enumerate(channel_chunks):
+        chunk_str = ','.join(chunk)
+        ti.xcom_push(key=f'channel_chunk_{idx}', value=chunk_str)
+
+with DAG(
+    dag_id='gen_channel_ranking_v2',
+    default_args=default_args,
+    description='This runs the channel ranking pipeline',
+    start_date=datetime(2024, 6, 21, 2),
+    schedule_interval='0 */6 * * *'
+) as dag:
+
+    fetch_data_task = BashOperator(
+        task_id='fetch_channel_data',
+        bash_command=f'cd /pipeline && {SHELL_SCRIPT} -w {PIPELINE_DIR} -v {VENV_DIR} -t fetch -c {CSV_PATH}',
+        do_xcom_push=True
+    )
+
+    extract_ids_task = PythonOperator(
+        task_id='extract_channel_ids',
+        python_callable=extract_channel_ids,
+        provide_context=True
+    )
+
+    fetch_data_task >> extract_ids_task
+
+    for i in range(N_CHUNKS):
+        process_channels_task = BashOperator(
+            task_id=f'process_channels_chunk_{i}',
+            bash_command=(
+                f'cd /pipeline && {SHELL_SCRIPT} -w {PIPELINE_DIR} -v {VENV_DIR} -t process -c {CSV_PATH} '
+                f'"{{{{ ti.xcom_pull(task_ids="extract_channel_ids", key="channel_chunk_{i}") }}}}"'
+            ),
+            env={'PYTHONUNBUFFERED': '1'}  # Ensures real-time logging
+        )
+        extract_ids_task >> process_channels_task
