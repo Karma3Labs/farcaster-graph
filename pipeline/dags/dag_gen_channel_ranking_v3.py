2930462d483350dad1be51d0df37440fa4f28808
SeungJu Lee <sj@karma3labs.com>
2024-07-03 09:47:35 +0000

    feat: add dag that backs up to s3. clean up.

--- pipeline/dags/dag_gen_channel_ranking_v3.py
+++ pipeline/dags/dag_gen_channel_ranking_v3.py
@@ -31,7 +31,7 @@ def extract_channel_ids(channel_ids: str) -> list:
     default_args=default_args,
     description='This runs the channel ranking pipeline',
     start_date=datetime(2024, 6, 21, 2),
-    schedule_interval='0 0 * * *',
+    schedule_interval='0 */6 * * *',
     catchup=False  # To avoid backfilling if not required
 )
 def create_dag():
