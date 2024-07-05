b66f50d9851f18df8e0b87f0ad648ff316450070
SeungJu Lee <sj@karma3labs.com>
2024-07-05 15:28:33 +0000

    feat: fix broken sql query for channel ranking

--- pipeline/channels/channel_queries.py
+++ pipeline/channels/channel_queries.py
@@ -1,7 +1,7 @@
 from db_utils import SQL
 
 class IJVSql(SQL):
-    LIKES_NEYNAR = """
+    LIKES_NEYNAR = SQL("LIKES_NEYNAR", """
     SELECT r.fid as i, r.target_fid as j, count(1) as likes_v
     FROM reactions r
     INNER JOIN casts c on c.hash = r.target_hash
@@ -9,16 +9,16 @@ class IJVSql(SQL):
     AND r.target_fid IS NOT null
     AND c.root_parent_url in ('{channel_url}')
     GROUP BY i, j
-    """
-    REPLIES = """
+    """)
+    REPLIES = SQL("REPLIES", """
     SELECT fid as i, parent_fid as j, count(1) as replies_v
     FROM casts
     WHERE parent_hash IS NOT NULL
     AND root_parent_url in ('{channel_url}')
     GROUP by i, j
-    """
+    """)
 
-    MENTIONS_NEYNAR = """
+    MENTIONS_NEYNAR = SQL("MENTIONS_NEYNAR", """
     WITH mention AS (
         SELECT fid as author_fid, mention as mention_fid
         FROM casts, unnest(casts.mentions) as mention
@@ -28,9 +28,9 @@ class IJVSql(SQL):
         author_fid as i, mention_fid as j, count(1) as mentions_v
     FROM mention
     GROUP BY i, j
-    """
+    """)
 
-    RECASTS_NEYNAR = """
+    RECASTS_NEYNAR = SQL("RECASTS_NEYNAR", """
     SELECT r.fid as i, r.target_fid as j, count(1) as recasts_v
     FROM reactions r
     INNER JOIN casts c on r.target_hash = c.hash
@@ -38,9 +38,9 @@ class IJVSql(SQL):
     AND r.target_fid IS NOT NULL
     AND c.root_parent_url in ('{channel_url}')
     GROUP BY i, j
-    """
+    """)
 
-    FOLLOWS_NEYNAR = """
+    FOLLOWS_NEYNAR = SQL("FOLLOWS_NEYNAR", """
     SELECT
         fid as i,
         target_fid as j,
@@ -48,9 +48,9 @@ class IJVSql(SQL):
     FROM links
     WHERE type = 'follow'::text
     ORDER BY i, j, follows_v desc
-    """
+    """)
 
-    COMBINED_INTERACTION = """
+    COMBINED_INTERACTION = SQL("COMBINED_INTERACTION", """
     WITH
         likes_neynar AS (
             SELECT
@@ -162,10 +162,10 @@ class IJVSql(SQL):
         recasts_neynar rn ON ln.i = rn.i AND ln.j = rn.j
     FULL OUTER JOIN
         follows_neynar fn ON ln.i = fn.i AND ln.j = fn.j
-    """
+    """)
 
 class IVSql(SQL):
-    PRETRUST_TOP_TIER = """
+    PRETRUST_TOP_TIER = SQL("PRETRUST_TOP_TIER", """
     WITH pt_size AS (
       select count(*) as ct from pretrust
       where insert_ts=(select max(insert_ts) from pretrust)
@@ -173,8 +173,8 @@ class IVSql(SQL):
     SELECT fid as i, 1/ct::numeric as v
     FROM pretrust, pt_size
     WHERE insert_ts=(select max(insert_ts) from pretrust)
-    """
-    PRETRUST_POPULAR = """
+    """)
+    PRETRUST_POPULAR = SQL("PRETRUST_POPULAR", """
     SELECT
         c.fid AS i,
         1/20::numeric as v
@@ -189,8 +189,8 @@ class IVSql(SQL):
     ORDER BY
         COUNT(*) DESC
     LIMIT 20
-    """
-    PRETRUST_OG = """
+    """)
+    PRETRUST_OG = SQL("PRETRUST_OG", """
     SELECT
         distinct fid as i,
         1/11::numeric as v
@@ -201,4 +201,4 @@ class IVSql(SQL):
                   'lesgreys.eth','linda','ace',
                   'vm','cdixon.eth')
         AND type=6
-    """
+    """)
