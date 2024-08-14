b98540d32abef6d5978fdb1397b50d957a2999a4
Vijay Mariadassou <vijay@karma3labs.com>
2024-08-13 18:37:02 -0700

    feat: wip: migrate globaltrust compute to eig5

--- pipeline/dags/dag_gen_globaltrust.py
+++ pipeline/dags/dag_gen_globaltrust.py
@@ -1,18 +1,19 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.ssh.hooks.ssh import SSHHook
 from hooks.discord import send_alert_discord
 from hooks.pagerduty import send_alert_pagerduty
 
 
 default_args = {
-    'owner': 'coder2j',
+    'owner': 'karma3labs',
     'retries': 5,
     'retry_delay': timedelta(minutes=2),
     'on_failure_callback': [send_alert_discord, send_alert_pagerduty],
 }
-
 
 with DAG(
     dag_id='gen_globaltrust_v0',
@@ -21,35 +22,33 @@ with DAG(
     start_date=datetime(2024, 6, 21, 2),
     schedule_interval='0 */6 * * *'
 ) as dag:
-    task1 = BashOperator(
-        task_id='run_globaltrust_pipeline.sh',
-        bash_command="cd /pipeline && ./run_globaltrust_pipeline.sh -w . -v ./.venv"
-    )
 
-    task2 = BashOperator(
-        task_id='insert_globaltrust_to_dune_v3',
-        bash_command="cd /pipeline/dags/pg_to_dune && ./upload_to_dune.sh insert_globaltrust_to_dune_v3 "
-    )
+    ssh_hook = SSHHook(ssh_conn_id='eigen5', keepalive_interval=60, cmd_timeout=None)
 
-    task1 >> task2
+    mkdir_tmp =  SSHOperator(
+        task_id="cleanup_tmp",
+        command= "cd ~/farcaster-graph/pipeline; mkdir -p tmp/{{ run_id }}",
+        ssh_hook=ssh_hook,
+        dag=dag)
 
-    # task2 = BashOperator(
-    #     task_id='second_task',
-    #     bash_command="echo hey, I am task2 and will be running after task1!"
-    # )
+    gen_globaltrust = SSHOperator(
+        task_id="run_globaltrust_pipeline.sh",
+        command= "cd ~/farcaster-graph/pipeline; ./run_globaltrust_pipeline.sh -w . -v ./.venv -o tmp/{{ run_id }}",
+        ssh_hook=ssh_hook,
+        dag=dag)
 
-    # task3 = BashOperator(
-    #     task_id='thrid_task',
-    #     bash_command="echo hey, I am task3 and will be running after task1 at the same time as task2!"
-    # )
+    # upload_to_dune =  SSHOperator(
+    #     task_id="insert_globaltrust_to_dune_v3.sh",
+    #     command= "cd ~/farcaster-graph/pipeline/dags/pg_to_dune; ./upload_to_dune.sh insert_globaltrust_to_dune_v3 ",
+    #     ssh_hook=ssh_hook,
+    #     dag=dag)
+    
+    rmdir_tmp =  SSHOperator(
+        task_id="cleanup_tmp",
+        command= "cd ~/farcaster-graph/pipeline; rm -rf tmp/{{ run_id }}",
+        ssh_hook=ssh_hook,
+        trigger_rule=TriggerRule.ONE_SUCCESS,
+        dag=dag)
 
-    # Task dependency method 1
-    # task1.set_downstream(task2)
-    # task1.set_downstream(task3)
-
-    # Task dependency method 2
-    # task1 >> task2
-    # task1 >> task3
-
-    # Task dependency method 3
-    # task1 >> [task2, task3]
+    # mkdir_tmp >> gen_globaltrust >> upload_to_dune >> rmdir_tmp.as_teardown(setups=mkdir_tmp)
+    mkdir_tmp >> gen_globaltrust >> rmdir_tmp.as_teardown(setups=mkdir_tmp)
