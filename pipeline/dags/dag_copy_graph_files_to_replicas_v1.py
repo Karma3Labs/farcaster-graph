ab4828da0592e370f3703451e5b11bc8c492b050
Vijay Mariadassou <vijay@karma3labs.com>
2024-08-15 21:22:10 -0700

    feat: wip: migrate jobs to use eigen5 replica

---
+++ pipeline/dags/dag_copy_graph_files_to_replicas_v1.py
@@ -0,0 +1,90 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.ssh.hooks.ssh import SSHHook
+
+from hooks.discord import send_alert_discord
+from hooks.pagerduty import send_alert_pagerduty
+
+default_args = {
+    "owner": "coder2j",
+    "retries": 5,
+    "retry_delay": timedelta(minutes=2),
+    "on_failure_callback": [send_alert_discord, send_alert_pagerduty],
+}
+
+eigen2_ipv4 = Variable.get("eigen2_ipv4")
+eigen4_ipv4 = Variable.get("eigen4_ipv4")
+eigen7_ipv4 = Variable.get("eigen7_ipv4")
+eigen5_ssh_cred_path = Variable.get("eigen5_ssh_cred_path")
+
+with DAG(
+    dag_id="dag_copy_graph_files_to_replicas_v1",
+    default_args=default_args,
+    description="re-generate graph for farcaster-graph API server. copy re-generated all graph files to eigen4 and eigen7 from eigen2",
+    start_date=datetime(2024, 7, 9, 18),
+    schedule_interval="20 1-23/6 * * *",
+    catchup=False,
+) as dag:
+    
+    # TODO need to refactor this dag to execute on Eigen6 and use BashOperator
+    ssh_hook = SSHHook(ssh_conn_id="eigen5", keepalive_interval=60, cmd_timeout=None)
+
+    run_graph_pipeline = SSHOperator(
+        task_id="run_graph_pipeline_v0",
+        command="cd ~/farcaster-graph/pipeline; ./run_graph_pipeline.sh -w . -i ~/graph_files -o ~/graph_files -v ./.venv ",
+        ssh_hook=ssh_hook,
+        dag=dag,
+    )
+
+    eigen2_copy_all_pkl_files = SSHOperator(
+        task_id="eigen2_copy_all_pkl_v0",
+        bash_command=f"scp -v -i {eigen5_ssh_cred_path} ~/graph_files/fc_*.pkl ubuntu@{eigen2_ipv4}:~/serve_files/",
+        ssh_hook=ssh_hook,
+        dag=dag,
+    )
+
+    eigen2_copy_success_pkl_files = SSHOperator(
+        task_id="eigen2_copy_success_pkl_v0",
+        bash_command=f"scp -v -i {eigen5_ssh_cred_path} ~/graph_files/fc_*_SUCCESS ubuntu@{eigen2_ipv4}:~/serve_files/",
+        ssh_hook=ssh_hook,
+        dag=dag,
+    )
+
+    eigen4_copy_all_pkl_files = SSHOperator(
+        task_id="eigen4_copy_all_pkl_v0",
+        bash_command=f"scp -v -i {eigen5_ssh_cred_path} ~/graph_files/fc_*.pkl ubuntu@{eigen4_ipv4}:~/serve_files/",
+        ssh_hook=ssh_hook,
+        dag=dag,
+    )
+
+    eigen4_copy_success_pkl_files = SSHOperator(
+        task_id="eigen4_copy_success_pkl_v0",
+        bash_command=f"scp -v -i {eigen5_ssh_cred_path} ~/graph_files/fc_*_SUCCESS ubuntu@{eigen4_ipv4}:~/serve_files/",
+        ssh_hook=ssh_hook,
+        dag=dag,
+    )
+
+    eigen7_copy_personal_pkl_files = SSHOperator(
+        task_id="eigen7_copy_personal_pkl_v0",
+        bash_command=f"scp -v -i {eigen5_ssh_cred_path} ~/graph_files/fc_engagement_fid_ig.pkl ubuntu@{eigen7_ipv4}:~/serve_files/",
+        ssh_hook=ssh_hook,
+        dag=dag,
+    )
+
+    eigen7_copy_localtrust_csv_files = SSHOperator(
+        task_id="eigen7_copy_localtrust_csv_v0",
+        # TODO stop renaming to lt_l1rep6rec3m12enhancedConnections_fid.csv and just call it engagement
+        bash_command=f"scp -v -i {eigen5_ssh_cred_path} ~/graph_files/localtrust.engagement.csv ubuntu@{eigen7_ipv4}:~/serve_files/lt_l1rep6rec3m12enhancedConnections_fid.csv",
+        ssh_hook=ssh_hook,
+        dag=dag,
+    )
+
+    run_graph_pipeline >> eigen2_copy_all_pkl_files >> eigen2_copy_success_pkl_files
+    run_graph_pipeline >> eigen4_copy_all_pkl_files >> eigen4_copy_success_pkl_files
+    run_graph_pipeline >> [
+        eigen7_copy_personal_pkl_files,
+        eigen7_copy_localtrust_csv_files,
+    ]
---
+++ pipeline/dags/dag_gen_globaltrust_v1.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.ssh.hooks.ssh import SSHHook
+from hooks.discord import send_alert_discord
+from hooks.pagerduty import send_alert_pagerduty
+
+
+default_args = {
+    'owner': 'karma3labs',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2),
+    'on_failure_callback': [send_alert_discord, send_alert_pagerduty],
+}
+
+with DAG(
+    dag_id='gen_globaltrust_v1',
+    default_args=default_args,
+    description='This runs run_globaltrust_pipeline.sh without any optimization',
+    start_date=datetime(2024, 6, 21, 2),
+    schedule_interval='0 */6 * * *'
+) as dag:
+
+    ssh_hook = SSHHook(ssh_conn_id='eigen5', keepalive_interval=60, cmd_timeout=None)
+
+    mkdir_tmp =  SSHOperator(
+        task_id="cleanup_tmp",
+        command= "cd ~/farcaster-graph/pipeline; mkdir -p tmp/{{ run_id }}",
+        ssh_hook=ssh_hook,
+        dag=dag)
+
+    gen_globaltrust = SSHOperator(
+        task_id="run_globaltrust_pipeline.sh",
+        command= "cd ~/farcaster-graph/pipeline; ./run_globaltrust_pipeline.sh -w . -v ./.venv -t tmp/{{ run_id }} -o ~/graph_files/",
+        ssh_hook=ssh_hook,
+        dag=dag)
+    
+    upload_to_dune =  SSHOperator(
+        task_id="insert_globaltrust_to_dune_v3.sh",
+        command= "cd ~/farcaster-graph/pipeline/dags/pg_to_dune; ./upload_to_dune.sh insert_globaltrust_to_dune_v4 ~/graph_files/",
+        ssh_hook=ssh_hook,
+        dag=dag)
+    
+    rmdir_tmp =  SSHOperator(
+        task_id="cleanup_tmp",
+        command= "cd ~/farcaster-graph/pipeline; rm -rf tmp/{{ run_id }}",
+        ssh_hook=ssh_hook,
+        trigger_rule=TriggerRule.ONE_SUCCESS,
+        dag=dag)
+
+    mkdir_tmp >> gen_globaltrust >> upload_to_dune >> rmdir_tmp.as_teardown(setups=mkdir_tmp)
