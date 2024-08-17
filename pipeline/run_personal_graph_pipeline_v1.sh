d6801c58dd4902db27087eebeffc5c3f0dfdf10b
Vijay Mariadassou <vijay@karma3labs.com>
2024-08-16 17:55:00 -0700

    fix: map_index for personal graph exceeds 1024

--- pipeline/run_personal_graph_pipeline_v1.sh
+++ pipeline/run_personal_graph_pipeline_v1.sh
@@ -73,7 +73,7 @@ if [ "$TASK" = "fetch_fids" ]; then
   source $VENV/bin/activate
   pip install -r requirements.txt
 
-  python3 -m graph.fetch_nodes_edges -i $IN_CSV -c 600 -o $TMP_GRAPH_PKL
+  python3 -m graph.fetch_nodes_edges -i $IN_CSV -c 1000 -o $TMP_GRAPH_PKL
 elif [ "$TASK" = "graph_reload" ]; then
   # TODO - Fix this ugly code
   # Reload twice because we have 2 instances of igraph server round robin load
