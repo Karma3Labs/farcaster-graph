c88be8945654e2b8b1e43fed72995d83cb41409e
Vijay Mariadassou <vijay@karma3labs.com>
2024-08-29 01:11:10 -0700

    fix: sync to neynar dag runs immediately on deployment causing overruns

--- pipeline/dags/dag_sync_sandbox_db.py
+++ pipeline/dags/dag_sync_sandbox_db.py
@@ -24,6 +24,7 @@ with DAG(
     description='sync the db table of the sandboxed read replica',
     start_date=datetime(2024, 7, 10, 18),
     schedule_interval='*/10 * * * *',
+    is_paused_upon_creation=True,
     catchup=False,
 ) as dag:
     ssh_hook = SSHHook(ssh_conn_id='eigen2', keepalive_interval=60, cmd_timeout=None)
