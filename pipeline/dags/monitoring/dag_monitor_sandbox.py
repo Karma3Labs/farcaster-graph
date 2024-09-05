5efbec2c3dc58c4cce96f0ead4d93bc4cc1fbc55
Vijay Mariadassou <vijay@karma3labs.com>
2024-09-05 15:42:56 -0700

    feat: monitor syncing to neynar'd prod sandbox

--- pipeline/dags/monitoring/dag_monitor_sandbox.py
+++ pipeline/dags/monitoring/dag_monitor_sandbox.py
@@ -6,7 +6,7 @@ from airflow.providers.common.sql.operators.sql import (
     # SQLCheckOperator,
     # SQLColumnCheckOperator,
     # SQLIntervalCheckOperator,
-    SQLTableCheckOperator,
+    # SQLTableCheckOperator,
     SQLThresholdCheckOperator,
     # SQLValueCheckOperator,
     # SQLExecuteQueryOperator,
@@ -15,7 +15,7 @@ from airflow.providers.common.sql.operators.sql import (
 from hooks.discord import send_alert_discord
 from hooks.pagerduty import send_alert_pagerduty
 
-_CONN_ID = "eig2_postgres_user"
+_CONN_ID = "sandbox_postgres_user"
 
 default_args = {
     "owner": "karma3labs",
@@ -29,12 +29,71 @@ with DAG(
     default_args=default_args,
     description="Monitor the status of sandbox and \
                 raise alerts if there are significant lags",
-    schedule_interval=timedelta(hours=1),
+    schedule_interval=timedelta(minutes=10),
     start_date=datetime(2024, 8, 1),
+    is_paused_upon_creation=True,
+    max_active_runs=1,
     catchup=False,
 ) as dag:
     start = EmptyOperator(task_id="start")
 
+    check_channel_rank = SQLThresholdCheckOperator(
+        task_id="check_channel_rank", # covers the channel_fids table also
+        conn_id=_CONN_ID,
+        sql="SELECT count(*) FROM k3l_channel_rank WHERE compute_ts > now() - interval '12 hours' AND strategy_name = 'channel_engagement'",
+        min_threshold=1_900_000
+    )
+
+    check_global_engagement_rank = SQLThresholdCheckOperator(
+        task_id="check_global_engagement_rank", # covers the globaltrust table also
+        conn_id=_CONN_ID,
+        sql="SELECT count(*) FROM k3l_rank WHERE date > now() - interval '1 days' AND strategy_name = 'engagement'",
+        min_threshold=700_000
+    )
+
+    check_parent_casts_count = SQLThresholdCheckOperator(
+        task_id="check_parent_casts_count", 
+        conn_id=_CONN_ID,
+        sql="SELECT COUNT(*) AS ct FROM k3l_recent_parent_casts WHERE timestamp > now() - interval '30 min'",
+        min_threshold=100
+    )
+
+    check_parent_casts_lag = SQLThresholdCheckOperator(
+        task_id="check_parent_casts_lag",
+        conn_id=_CONN_ID,
+        sql="""SELECT
+	            (EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - max(timestamp)))/60)::integer as diff_mins
+                FROM k3l_recent_parent_casts""",
+        min_threshold=0,
+        max_threshold=30
+    )
+
+    check_cast_actions_count = SQLThresholdCheckOperator(
+        task_id="check_cast_actions_count", 
+        conn_id=_CONN_ID,
+        sql="SELECT COUNT(*) AS ct FROM k3l_cast_action WHERE action_ts > now() - interval '30 min'",
+        min_threshold=10_000
+    )
+
+    check_cast_actions_lag = SQLThresholdCheckOperator(
+        task_id="check_cast_actions_lag",
+        conn_id=_CONN_ID,
+        sql="""SELECT
+	            (EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - max(action_ts)))/60)::integer as diff_mins
+                FROM k3l_cast_action""",
+        min_threshold=0,
+        max_threshold=30
+    )
+
     end = EmptyOperator(task_id="end")
 
-    start >> end
+    (
+        start
+        >> check_channel_rank
+        >> check_global_engagement_rank
+        >> check_parent_casts_count
+        >> check_parent_casts_lag
+        >> check_cast_actions_count
+        >> check_cast_actions_lag
+        >> end
+    )
