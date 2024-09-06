6a1de34dcb3dca8c436f9ef69f21e74d63727bee
SeungJu Lee <sj@karma3labs.com>
2024-09-06 23:50:37 +0000

    feat: update copy_graph_files_to_sandbox_dev script to download pqt file instead

--- pipeline/dags/dag_copy_graph_files_to_sandbox_dev_v1.py
+++ pipeline/dags/dag_copy_graph_files_to_sandbox_dev_v1.py
@@ -17,13 +17,13 @@ default_args = {
     "on_failure_callback": [send_alert_discord, send_alert_pagerduty],
 }
 
-sandbox_dev_host = Variable.get("sandbox_dev_host")
-sandbox_dev_ssh_cred_path = Variable.get("sandbox_dev_ssh_cred_path")
+dev_sandbox_pipeline_path = Variable.get("dev_sandbox_pipeline_path")
+data_backup_s3_bucket = Variable.get("data_backup_s3_bucket")
 
 with DAG(
-    dag_id="copy_graph_files_to_sandbox_dev_v1",
+    dag_id="copy_graph_files_to_sandbox_dev_v2",
     default_args=default_args,
-    description="re-generate graph for farcaster-graph API server. copy re-generated all graph files to dev sandbox from eigen2",
+    description="re-generate graph for farcaster-graph API server. copy re-generated all graph files to dev sandbox from backup s3",
     start_date=datetime(2024, 7, 9, 18),
     schedule_interval="0 0 * * *",
     is_paused_upon_creation=True,
@@ -31,28 +31,13 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    ssh_hook = SSHHook(ssh_conn_id='eigen6', keepalive_interval=60, cmd_timeout=None)
+    ssh_hook = SSHHook(ssh_conn_id='sandbox_staging', keepalive_interval=60, cmd_timeout=None)
 
-    run_graph_pipeline = BashOperator(
-        task_id="run_graph_pipeline",
-        bash_command="cd /pipeline; ./run_graph_pipeline.sh -w . -i tmp/graph_files/ -o tmp/graph_files/ -v ./.venv ",
-        dag=dag,
-    )
-
-    dev_copy_all_pkl_files = SSHOperator(
-        task_id="dev_copy_all_pkl_files",
-        command=f"scp -v -i {sandbox_dev_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*.pkl ubuntu@{sandbox_dev_host}:/data/serve_files/",
+    download_pqt_file = SSHOperator(
+        task_id="download_pqt_file_v1",
+        command=f"cd {dev_sandbox_pipeline_path}; ./run_graph_pipeline.sh -o /data/serve_files -s {data_backup_s3_bucket} ",
         ssh_hook=ssh_hook,
         dag=dag,
     )
 
-    dev_copy_success_pkl_files = SSHOperator(
-        task_id="dev_copy_success_pkl_files",
-        command=f"scp -v -i {sandbox_dev_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*_SUCCESS ubuntu@{sandbox_dev_host}:/data/serve_files/",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
-
-
-
-    run_graph_pipeline >> dev_copy_all_pkl_files >> dev_copy_success_pkl_files
+    download_pqt_file
