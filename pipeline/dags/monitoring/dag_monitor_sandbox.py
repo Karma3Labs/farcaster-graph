97fb5dd7fca5cfd8a7a1deaba19605861f1f8afd
Vijay Mariadassou <vijay@karma3labs.com>
2024-09-12 12:07:52 -0700

    fix: cast action sync lags can be 30 plus mins and false alerts are raised

--- pipeline/dags/monitoring/dag_monitor_sandbox.py
+++ pipeline/dags/monitoring/dag_monitor_sandbox.py
@@ -80,7 +80,7 @@ with DAG(
     check_cast_actions_count = SQLThresholdCheckOperator(
         task_id="check_cast_actions_count",
         conn_id=_CONN_ID,
-        sql="SELECT count(*) FROM k3l_cast_action WHERE action_ts > now() - interval '30 min'",
+        sql="SELECT count(*) FROM k3l_cast_action WHERE action_ts > now() - interval '45 min'",
         min_threshold=10_000,
         max_threshold=100_000_000# some arbitrarily large number
     )
@@ -92,7 +92,7 @@ with DAG(
 	            (EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - max(action_ts)))/60)::integer as diff_mins
                 FROM k3l_cast_action""",
         min_threshold=0,
-        max_threshold=30
+        max_threshold=45
     )
 
     end = EmptyOperator(task_id="end")
