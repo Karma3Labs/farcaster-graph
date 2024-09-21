47228fa1c2148228c94c697cca060a11f9409e01
Vijay Mariadassou <vijay@karma3labs.com>
2024-09-20 18:33:19 -0700

    fix: upload channel top casters fails to import into airflow

--- pipeline/dags/reports/dag_upload_channel_top_casters.py
+++ pipeline/dags/reports/dag_upload_channel_top_casters.py
@@ -17,21 +17,4 @@ default_args = {
 PIPELINE_DIR = './'
 CSV_PATH = f'{PIPELINE_DIR}/channels/Top_Channels.csv'
 
-with DAG(
-    dag_id='report_top_channel_casters',
-    default_args=default_args,
-    description='This backs up top caster everyday for channels into postgres db',
-    start_date=datetime(2024, 8, 15),
-    schedule_interval='30 20 * * *',
-    is_paused_upon_creation=True,
-    max_active_runs=1,
-    catchup=False,
-) as dag:
-
-    fetch_dune_s3_upload_top_channel_casters = BashOperator(
-        task_id='fetch_dune_s3_upload_top_channel_casters',
-        bash_command="cd /pipeline && ./run_fetch_top_channel_caster.sh -v ./.venv -c {CSV_PATH}"
-    )
-
-    fetch_dune_s3_upload_top_casters
 
