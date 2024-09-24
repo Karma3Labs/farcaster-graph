110a2f600c9c2706e7dc25bb30176de5a429629e
Vijay Mariadassou <vijay@karma3labs.com>
2024-09-24 11:08:12 -0700

    feat: 90day graph

--- pipeline/dags/dag_copy_graph_files_to_replicas_v1.py
+++ pipeline/dags/dag_copy_graph_files_to_replicas_v1.py
@@ -5,7 +5,7 @@ from airflow.models import Variable
 from airflow.operators.bash import BashOperator
 from airflow.providers.ssh.operators.ssh import SSHHook
 from airflow.providers.ssh.operators.ssh import SSHOperator
-from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.decorators import task_group
 
 from hooks.discord import send_alert_discord
 from hooks.pagerduty import send_alert_pagerduty
@@ -37,73 +37,98 @@ with DAG(
 
     ssh_hook = SSHHook(ssh_conn_id='eigen6', keepalive_interval=60, cmd_timeout=None)
 
-    run_graph_pipeline = BashOperator(
-        task_id="run_graph_pipeline",
-        bash_command="cd /pipeline; ./run_graph_pipeline.sh -w . -i tmp/graph_files/ -o tmp/graph_files/ -v ./.venv ",
-        dag=dag,
-    )
+    @task_group(group_id="tg_gen_graphs")
+    def tg_gen_graphs():
+        gen_following_graph = BashOperator(
+            task_id="gen_following_graph",
+            bash_command="cd /pipeline; ./run_graph_pipeline.sh -w . -v ./.venv"
+                            " -i tmp/graph_files/localtrust.following.csv -o tmp/graph_files/ -p fc_following_fid ",
+            dag=dag,
+        )
 
-    eigen2_copy_all_pkl_files = SSHOperator(
-        task_id="eigen2_copy_all_pkl_files",
-        command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*.pkl ubuntu@{eigen2_ipv4}:~/serve_files/",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
+        gen_engagement_graph = BashOperator(
+            task_id="gen_engagement_graph",
+            bash_command="cd /pipeline; ./run_graph_pipeline.sh -w . -v ./.venv"
+                            " -i tmp/graph_files/localtrust.engagement.csv -o tmp/graph_files/ -p fc_engagement_fid ",
+            dag=dag,
+        )
 
-    eigen2_copy_success_pkl_files = SSHOperator(
-        task_id="eigen2_copy_success_pkl_files",
-        command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*_SUCCESS ubuntu@{eigen2_ipv4}:~/serve_files/",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
+        gen_90day_graph = BashOperator(
+            task_id="gen_90day_graph",
+            bash_command="cd /pipeline; ./run_graph_pipeline.sh -w . -v ./.venv"
+                            " -i tmp/graph_files/localtrust.graph_90dv3.csv -o tmp/graph_files/ -p fc_90dv3_fid ",
+            dag=dag,
+        )
 
-    eigen4_copy_all_pkl_files = SSHOperator(
-        task_id="eigen4_copy_all_pkl_files",
-        command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*.pkl ubuntu@{eigen4_ipv4}:~/serve_files/",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
+        gen_following_graph >> gen_engagement_graph >> gen_90day_graph
 
-    eigen4_copy_success_pkl_files = SSHOperator(
-        task_id="eigen4_copy_success_pkl_files",
-        command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*_SUCCESS ubuntu@{eigen4_ipv4}:~/serve_files/",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
+    # end tg_gen_graphs
 
-    eigen5_copy_all_pkl_files = SSHOperator(
-        task_id="eigen5_copy_all_pkl_files",
-        command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*.pkl ubuntu@{eigen5_ipv4}:~/serve_files/",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
+    @task_group(group_id="tg_copy_graphs")
+    def tg_copy_graphs():
+        eigen2_copy_all_pkl_files = SSHOperator(
+            task_id="eigen2_copy_all_pkl_files",
+            command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*.pkl ubuntu@{eigen2_ipv4}:~/serve_files/",
+            ssh_hook=ssh_hook,
+            dag=dag,
+        )
 
-    eigen5_copy_success_pkl_files = SSHOperator(
-        task_id="eigen5_copy_success_pkl_files",
-        command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*_SUCCESS ubuntu@{eigen5_ipv4}:~/serve_files/",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
+        eigen2_copy_success_pkl_files = SSHOperator(
+            task_id="eigen2_copy_success_pkl_files",
+            command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*_SUCCESS ubuntu@{eigen2_ipv4}:~/serve_files/",
+            ssh_hook=ssh_hook,
+            dag=dag,
+        )
 
-    eigen7_copy_personal_pkl_files = SSHOperator(
-        task_id="eigen7_copy_personal_pkl_files",
-        command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_engagement_fid_ig.pkl ubuntu@{eigen7_ipv4}:~/serve_files/",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
+        eigen4_copy_all_pkl_files = SSHOperator(
+            task_id="eigen4_copy_all_pkl_files",
+            command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*.pkl ubuntu@{eigen4_ipv4}:~/serve_files/",
+            ssh_hook=ssh_hook,
+            dag=dag,
+        )
 
-    eigen7_copy_localtrust_csv_files = SSHOperator(
-        task_id="eigen7_copy_localtrust_csv_files",
-        # TODO stop renaming to lt_l1rep6rec3m12enhancedConnections_fid.csv and just call it engagement
-        command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/localtrust.engagement.csv ubuntu@{eigen7_ipv4}:~/serve_files/lt_l1rep6rec3m12enhancedConnections_fid.csv",
-        ssh_hook=ssh_hook,
-        dag=dag,
-    )
+        eigen4_copy_success_pkl_files = SSHOperator(
+            task_id="eigen4_copy_success_pkl_files",
+            command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*_SUCCESS ubuntu@{eigen4_ipv4}:~/serve_files/",
+            ssh_hook=ssh_hook,
+            dag=dag,
+        )
 
-    run_graph_pipeline >> eigen2_copy_all_pkl_files >> eigen2_copy_success_pkl_files
-    run_graph_pipeline >> eigen4_copy_all_pkl_files >> eigen4_copy_success_pkl_files
-    run_graph_pipeline >> eigen5_copy_all_pkl_files >> eigen5_copy_success_pkl_files
-    run_graph_pipeline >> [
-        eigen7_copy_personal_pkl_files,
-        eigen7_copy_localtrust_csv_files,
-    ]
+        eigen5_copy_all_pkl_files = SSHOperator(
+            task_id="eigen5_copy_all_pkl_files",
+            command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*.pkl ubuntu@{eigen5_ipv4}:~/serve_files/",
+            ssh_hook=ssh_hook,
+            dag=dag,
+        )
+
+        eigen5_copy_success_pkl_files = SSHOperator(
+            task_id="eigen5_copy_success_pkl_files",
+            command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_*_SUCCESS ubuntu@{eigen5_ipv4}:~/serve_files/",
+            ssh_hook=ssh_hook,
+            dag=dag,
+        )
+
+        eigen7_copy_personal_pkl_files = SSHOperator(
+            task_id="eigen7_copy_personal_pkl_files",
+            command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/fc_engagement_fid_ig.pkl ubuntu@{eigen7_ipv4}:~/serve_files/",
+            ssh_hook=ssh_hook,
+            dag=dag,
+        )
+
+        eigen7_copy_localtrust_csv_files = SSHOperator(
+            task_id="eigen7_copy_localtrust_csv_files",
+            # TODO stop renaming to lt_l1rep6rec3m12enhancedConnections_fid.csv and just call it engagement
+            command=f"scp -v -i {eigen6_ssh_cred_path} ~/farcaster-graph/pipeline/tmp/graph_files/localtrust.engagement.csv ubuntu@{eigen7_ipv4}:~/serve_files/lt_l1rep6rec3m12enhancedConnections_fid.csv",
+            ssh_hook=ssh_hook,
+            dag=dag,
+        )
+
+        eigen2_copy_all_pkl_files >> eigen2_copy_success_pkl_files
+        eigen4_copy_all_pkl_files >> eigen4_copy_success_pkl_files
+        eigen5_copy_all_pkl_files >> eigen5_copy_success_pkl_files
+        eigen7_copy_personal_pkl_files
+        eigen7_copy_localtrust_csv_files
+
+    # end tg_copy_graphs
+
+    tg_gen_graphs() >> tg_copy_graphs()
--- pipeline/run_graph_pipeline.sh
+++ pipeline/run_graph_pipeline.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-while getopts i:w:o:v: flag
+while getopts i:o:p:w:v: flag
 do
     case "${flag}" in
-        i) IN_DIR=${OPTARG};;
+        i) IN_FILE=${OPTARG};;
         o) OUT_DIR=${OPTARG};;
+        p) OUT_PREFIX=${OPTARG};;
         w) WORK_DIR=${OPTARG};;
         v) VENV=${OPTARG};;
     esac
 done
 
-if [ -z "$WORK_DIR" ] || [ -z "$OUT_DIR" ] || [ -z "$IN_DIR" ] || [ -z "$VENV" ]; then
-  echo "Usage:   $0 -w [work_dir] -i [in_dir] -o [out_dir] -v [venv]"
+if [ -z "$IN_FILE" ] || [ -z "$OUT_DIR" ] || [ -z "$OUT_PREFIX" ] || [ -z "$WORK_DIR" ] || [ -z "$VENV" ]; then
+  echo "Usage:   $0 -w [work_dir]  -v [venv] -i [in_file] -o [out_dir] -p [out_prefix]"
   echo ""
-  echo "Example: $0 -w . -i /tmp -o /tmp -v /home/ubuntu/venvs/fc-graph-env3/"
+  echo "Example: $0 -w . -v /home/ubuntu/venvs/fc-graph-env3/ -i /tmp -o /tmp -p test"
   echo ""
   echo "Params:"
   echo "  [work_dir]  The working directory to read .env file and execute scripts from."
-  echo "  [in_dir] The input directory to read the csv files."
-  echo "  [out_dir] The output directory to write the graph file."
   echo "  [venv] The path where a python3 virtualenv has been created."
+  echo "  [in_file] The input localtrust (i,j,v edge list) csv file."
+  echo "  [out_dir] The output directory to write the graph file."
+  echo "  [out_prefix] The prefix of the output graph files."
   echo ""
   exit
 fi
@@ -39,8 +41,6 @@ mkdir -p $OUT_DIR
 
 source $VENV/bin/activate
 pip install -r requirements.txt
-python3 -m graph.gen_igraph -i $IN_DIR/localtrust.following.csv -o $OUT_DIR -p fc_following_fid
-touch $OUT_DIR/fc_following_fid_SUCCESS
-python3 -m graph.gen_igraph -i $IN_DIR/localtrust.engagement.csv -o $OUT_DIR -p fc_engagement_fid
-touch $OUT_DIR/fc_engagement_fid_SUCCESS
+python3 -m graph.gen_igraph -i $IN_FILE -o $OUT_DIR -p $OUT_PREFIX
+touch $OUT_DIR/${OUT_PREFIX}_SUCCESS
 deactivate
