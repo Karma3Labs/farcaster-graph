87522fa58ec4b72525e9ca1d49e25dbadbc9b62c
Vijay Mariadassou <vijay@karma3labs.com>
2024-09-23 17:41:59 -0700

    test: placeholder to test airflow features before adding to prod dag

---
+++ pipeline/dags/one_off/dag_trial.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+
+
+default_args = {
+    "owner": "karma3labs",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "one_off_try_airflow_features",
+    default_args=default_args,
+    description="One off dag to test new features",
+    schedule_interval=None,
+    start_date=None,
+    is_paused_upon_creation=True,
+    max_active_runs=1,
+    catchup=False,
+) as dag:
+    start = EmptyOperator(task_id="start")
+
+    echo =  BashOperator(
+        task_id="echo", 
+        bash_command= "echo {{ logical_date - timedelta(days=90) }}",
+        dag=dag
+    )
+
+
+    end = EmptyOperator(task_id="end")
+
+    (
+        start
+        >> echo
+        >> end
+    )
