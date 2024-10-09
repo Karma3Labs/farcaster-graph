34945753e7058a2e505bc3bbdc1ceb66364bd9f1
Vijay Mariadassou <vijay@karma3labs.com>
2024-10-09 11:41:50 -0700

    fix: warpcast members dag id is incorrectly named as followers

--- pipeline/dags/extractors/dag_warpcast_channel_members.py
+++ pipeline/dags/extractors/dag_warpcast_channel_members.py
@@ -14,9 +14,9 @@ default_args = {
 }
 
 with DAG(
-    "extract_warpcast_followers",
+    "extract_warpcast_members",
     default_args=default_args,
-    description="Fetch channel followers from WARPCAST API and load into DB daily",
+    description="Fetch channel members from WARPCAST API and load into DB daily",
     schedule_interval=timedelta(days=1),
     start_date=datetime(2024, 8, 1),
     is_paused_upon_creation=True,
@@ -24,7 +24,7 @@ with DAG(
     catchup=False,
 ) as dag:
     task1 = BashOperator(
-        task_id='extract_channel_followers',
+        task_id='extract_warpcast_members',
         bash_command="cd /pipeline; extractors/extract_channel_fids.sh" 
                         " -w . -v .venv -c channels/Top_Channels.csv -s top -j members",
         dag=dag
