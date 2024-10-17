937527ad6d3497fbf65d4b6932d1d06a2d44308f
Vijay Mariadassou <vijay@karma3labs.com>
2024-10-17 12:41:58 -0700

    refactor: channel ranking cleanup is actually refreshing db

--- pipeline/dags/dag_gen_channel_ranking_v3.py
+++ pipeline/dags/dag_gen_channel_ranking_v3.py
@@ -69,8 +69,8 @@ def create_dag():
 
     process_7d_tasks = process_channel_chunk.expand(chunk=extract_ids_task, interval=7)
 
-    cleanup_db_task = BashOperator(
-        task_id='cleanup_db',
+    refresh_db_task = BashOperator(
+        task_id='refresh_db',
         bash_command="cd /pipeline && ./run_channel_scraper_v3.sh -w . -v .venv -t refresh -c channels/Top_Channels.csv",
         trigger_rule=TriggerRule.ALL_SUCCESS
     )
@@ -85,7 +85,7 @@ def create_dag():
         bash_command="cd /pipeline/dags/pg_to_dune && ./upload_to_dune.sh upload_channel_rank_to_s3"
     )
 
-    fetch_data_task >> extract_ids_task >> process_lifetime_tasks >> process_60d_tasks >> process_7d_tasks >> cleanup_db_task >> push_to_dune_task >> push_to_s3_task
+    fetch_data_task >> extract_ids_task >> process_lifetime_tasks >> process_60d_tasks >> process_7d_tasks >> refresh_db_task >> push_to_dune_task >> push_to_s3_task
 
 
 dag = create_dag()
