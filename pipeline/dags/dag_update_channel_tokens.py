78371e6ab7fe10def400798a5ec259fd403018ec
Vijay Mariadassou <vijay@karma3labs.com>
2024-12-23 09:25:44 -0800

    fix: channel tokens and points dag is not scheduled

--- pipeline/dags/dag_update_channel_tokens.py
+++ pipeline/dags/dag_update_channel_tokens.py
@@ -26,7 +26,6 @@ with DAG(
     default_args=default_args,
     description='update channel tokens started by trigger dag or manually',
     start_date=datetime(2024, 7, 10, 18),
-    schedule=None, # this dag is triggered by dag trigger_channel_points_tokens
     # schedule_interval='0 0 * * *', # every day at 00:00 UTC / 16:00 PST 
     schedule_interval=timedelta(hours=1),
     is_paused_upon_creation=True,
