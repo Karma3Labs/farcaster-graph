615dd8eb5a3d3938b239c37c522c76c5d3fa65ea
Vijay <vijay@karma3labs.com>
2025-03-04 11:53:21 -0800

    feat: monitor replication on eigen8

--- pipeline/dags/monitoring/dag_monitor_replication.py
+++ pipeline/dags/monitoring/dag_monitor_replication.py
@@ -16,6 +16,7 @@ from hooks.discord import send_alert_discord
 from hooks.pagerduty import send_alert_pagerduty
 
 _CONN_ID = "eig2_postgres_user"
+_ALT_CONN_ID = "eig8_postgres_user"
 
 default_args = {
     "owner": "karma3labs",
@@ -49,9 +50,33 @@ with DAG(
         },
     )
 
+    check_wal_status8 = SQLTableCheckOperator(
+        task_id="check_wal_status8",
+        conn_id=_ALT_CONN_ID,
+        table="pg_replication_slots",
+        checks={
+            "row_count_check": {
+                "check_statement": "COUNT(*) = 2",
+                "partition_clause": "wal_status='reserved'"
+            }
+        },
+    )
+
     check_replication_state = SQLTableCheckOperator(
         task_id="check_replication_state",
         conn_id=_CONN_ID,
+        table="pg_stat_replication",
+        checks={
+            "row_count_check": {
+                "check_statement": "COUNT(*) = 2",
+                "partition_clause": "state='streaming'"
+            }
+        },
+    )
+
+    check_replication_state8 = SQLTableCheckOperator(
+        task_id="check_replication_state8",
+        conn_id=_ALT_CONN_ID,
         table="pg_stat_replication",
         checks={
             "row_count_check": {
@@ -69,6 +94,15 @@ with DAG(
         max_threshold=60, # fail task if more than 60 minutes of lag
     )
 
+    lag_check8 = SQLThresholdCheckOperator(
+        task_id="lag_check8",
+        conn_id=_ALT_CONN_ID,
+        sql="SELECT round(EXTRACT(epoch FROM max(replay_lag))/60) FROM pg_stat_replication",
+        min_threshold=0,
+        max_threshold=60, # fail task if more than 60 minutes of lag
+    )
+
     end = EmptyOperator(task_id="end")
 
     start >> check_wal_status >> check_replication_state >> lag_check >> end
+    start >> check_wal_status8 >> check_replication_state8 >> lag_check8 >> end
