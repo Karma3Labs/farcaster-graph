15255894cfe8f62125c75107ff7960bcc7f7e07f
Vijay <vijay@karma3labs.com>
2025-05-02 10:14:01 -0700

    feat: monitor for lags in cast from neynar indexer

---
+++ pipeline/dags/monitoring/dag_monitor_nindexer.py
@@ -0,0 +1,69 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.common.sql.operators.sql import (
+    # SQLCheckOperator,
+    # SQLColumnCheckOperator,
+    # SQLIntervalCheckOperator,
+    # SQLTableCheckOperator,
+    SQLThresholdCheckOperator,
+    # SQLValueCheckOperator,
+    # SQLExecuteQueryOperator,
+)
+
+from hooks.discord import send_alert_discord
+from hooks.pagerduty import send_alert_pagerduty
+
+_ALT_CONN_ID = "eig8_postgres_user"
+
+default_args = {
+    "owner": "karma3labs",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    'on_failure_callback': [send_alert_discord, send_alert_pagerduty],
+}
+
+with DAG(
+    "monitor_nindexer",
+    default_args=default_args,
+    description="Monitor the status of replication and \
+                raise alerts if there are significant lags",
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2024, 8, 1),
+    is_paused_upon_creation=True,
+    max_active_runs=1,
+    catchup=False,
+) as dag:
+    start = EmptyOperator(task_id="start")
+
+    lag_casts_at = SQLThresholdCheckOperator(
+        task_id="lag_casts_at",
+        conn_id=_ALT_CONN_ID,
+        sql="""
+        SELECT 
+	        EXTRACT(EPOCH FROM (now() - max(created_at))) as max_createdat_lag_ms
+        FROM neynarv2.casts
+        WHERE timestamp > now() - interval '1 days'
+        """,
+        min_threshold=0,
+        max_threshold=600, # fail task if more than 10 minutes of lag
+    )
+
+    lag_casts_ts = SQLThresholdCheckOperator(
+        task_id="lag_casts_ts",
+        conn_id=_ALT_CONN_ID,
+        sql="""
+        SELECT 
+	        EXTRACT(EPOCH FROM (now() - max(timestamp))) as max_ts_lag_ms
+        FROM neynarv2.casts
+        WHERE timestamp > now() - interval '1 days'
+        """,
+        min_threshold=0,
+        max_threshold=600, # fail task if more than 10 minutes of lag
+    )
+
+
+    end = EmptyOperator(task_id="end")
+
+    start >> lag_casts_at >> lag_casts_ts >> end
