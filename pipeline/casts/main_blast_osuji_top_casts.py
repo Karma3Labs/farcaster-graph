5b4ad8dbdf06cb25753728e1ae454c0811c4bd63
atlasmoth <ebuka422@gmail.com>
2025-05-23 17:42:12 +0100

    testing out my osuji.eth dags

---
+++ pipeline/casts/main_blast_osuji_top_casts.py
@@ -0,0 +1,44 @@
+# standard dependencies
+import sys
+import niquests
+
+from config import settings
+from . import cast_db_utils
+
+# 3rd party dependencies
+from dotenv import load_dotenv
+from loguru import logger
+
+logger.remove()
+level_per_module = {
+    "": settings.LOG_LEVEL,
+    "silentlib": False
+}
+logger.add(sys.stdout,
+           colorize=True,
+           format=settings.LOGURU_FORMAT,
+           filter=level_per_module,
+           level=0)
+
+def main():
+    pg_dsn = settings.ALT_POSTGRES_DSN.get_secret_value()
+    df = cast_db_utils.fetch_osuji_top_casts_df(logger, pg_dsn)
+    message = "These are @osuji.eth's top casts 🚀:\n" + "\n".join([
+        f"{i + 1} - {row['url']}" for i, row in df.iterrows()
+    ])
+    response = niquests.post(
+        "https://top-erc20-tokens.vercel.app/api",
+        headers={"Content-Type": "application/json"},
+        json={"message": message}
+    )
+    response.raise_for_status()
+
+    logger.info("Osuji.eth's top casts successfully blasted")
+
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    logger.info(settings)
+    logger.info("starting to blast all Osuji.eth's top casts")
+    main()
---
+++ pipeline/run_blast_osuji_top_casts.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+while getopts w:v: flag
+do
+    case "${flag}" in
+        w) WORK_DIR=${OPTARG};;
+        v) VENV=${OPTARG};;
+    esac
+done
+
+
+if [ -z "$WORK_DIR" ] || [ -z "$VENV" ]; then
+  echo "Usage:   $0 -w [work_dir] -v [venv]"
+  echo ""
+  echo "Example: $0 -w . -v /home/ubuntu/venvs/fc-graph-env3/ "
+  echo ""
+  echo "Params:"
+  echo "  [work_dir] The working directory to read .env file and execute scripts from."
+  echo "  [venv] The path where a python3 virtualenv has been created."
+  echo ""
+  exit
+fi
+
+
+source $WORK_DIR/.env
+
+
+
+# set -x
+set -e
+set -o pipefail
+
+function log() {
+  echo "`date` - $1"
+}
+
+
+source $VENV/bin/activate
+python3 -m casts.main_blast_osuji_top_casts
+deactivate
