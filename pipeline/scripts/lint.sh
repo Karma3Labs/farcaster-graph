7e07a2ff68014fb7f7fe562c8b29bdaf83539234
Eugene Kim <ek@karma3labs.com>
2025-10-08 22:40:48 -0700

    style: spelling

--- pipeline/scripts/lint.sh
+++ pipeline/scripts/lint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-unset -v tooldir opt
+unset -v tool_dir opt
 OPTIND=1
 while getopts :b: opt
 do
 	case "${opt}" in
 		'?') echo "unrecognized option -${OPTARG}" >&2; exit 64;;
 		':') echo "missing argument for -${OPTARG}" >&2; exit 64;;
-		b) tooldir="${OPTARG}";;
+		b) tool_dir="${OPTARG}";;
 		*) echo "unhandled option -${opt}" >&2; exit 70;;
 	esac
 done
 shift $((OPTIND - 1))
-case "${tooldir+set}" in
-	set) PATH="${tooldir}${PATH+":${PATH}"}"; export PATH;;
+case "${tool_dir+set}" in
+	set) PATH="${tool_dir}${PATH+":${PATH}"}"; export PATH;;
 esac
 case $# in
 	0)
