7de49aee607a48c5e6213baacf364b597a8e28ba
Eugene Kim <ek@karma3labs.com>
2025-10-30 23:32:30 -0700

    fix: don't monitor replication state on eigen2
    eigen2 is no longer updated.

--- pipeline/dags/monitoring/dag_monitor_replication.py
+++ pipeline/dags/monitoring/dag_monitor_replication.py
@@ -32,18 +32,6 @@ with DAG(
 ) as dag:
     start = EmptyOperator(task_id="start")
 
-    check_wal_status = SQLTableCheckOperator(
-        task_id="check_wal_status",
-        conn_id=_CONN_ID,
-        table="pg_replication_slots",
-        checks={
-            "row_count_check": {
-                "check_statement": "COUNT(*) = 2",
-                "partition_clause": "wal_status='reserved'",
-            }
-        },
-    )
-
     check_wal_status8 = SQLTableCheckOperator(
         task_id="check_wal_status8",
         conn_id=_ALT_CONN_ID,
@@ -52,18 +40,6 @@ with DAG(
             "row_count_check": {
                 "check_statement": "COUNT(*) = 2",
                 "partition_clause": "wal_status='reserved'",
-            }
-        },
-    )
-
-    check_replication_state = SQLTableCheckOperator(
-        task_id="check_replication_state",
-        conn_id=_CONN_ID,
-        table="pg_stat_replication",
-        checks={
-            "row_count_check": {
-                "check_statement": "COUNT(*) = 2",
-                "partition_clause": "state='streaming'",
             }
         },
     )
@@ -80,14 +56,6 @@ with DAG(
         },
     )
 
-    lag_check = SQLThresholdCheckOperator(
-        task_id="lag_check",
-        conn_id=_CONN_ID,
-        sql="SELECT round(EXTRACT(epoch FROM max(replay_lag))/60) FROM pg_stat_replication",
-        min_threshold=0,
-        max_threshold=60,  # fail task if more than 60 minutes of lag
-    )
-
     lag_check8 = SQLThresholdCheckOperator(
         task_id="lag_check8",
         conn_id=_ALT_CONN_ID,
@@ -98,5 +66,4 @@ with DAG(
 
     end = EmptyOperator(task_id="end")
 
-    start >> check_wal_status >> check_replication_state >> lag_check >> end
     start >> check_wal_status8 >> check_replication_state8 >> lag_check8 >> end
