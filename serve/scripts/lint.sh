12e46a02d8931e0b21a148dcb261419b29b2fe6f
Eugene Kim <ek@karma3labs.com>
2025-10-31 04:11:20 -0700

    style[serve]: use Ruff instead of Black + isort

--- serve/scripts/lint.sh
+++ serve/scripts/lint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 unset -v tool_dir verbose opt
-verbose=false
+verbose=
 OPTIND=1
-while getopts :b:v opt
+while getopts :b:vqs opt
 do
 	case "${opt}" in
 		'?') echo "unrecognized option -${OPTARG}" >&2; exit 64;;
 		':') echo "missing argument for -${OPTARG}" >&2; exit 64;;
 		b) tool_dir="${OPTARG}";;
-		v) verbose=true;;
+		v) verbose=--verbose;;
+		q) verbose=--quiet;;
+		s) verbose=--silent;;
 		*) echo "unhandled option -${opt}" >&2; exit 70;;
 	esac
 done
@@ -17,20 +19,9 @@ case "${tool_dir+set}" in
 	set) PATH="${tool_dir}${PATH+":${PATH}"}"; export PATH;;
 esac
 case $# in
-        0)
-                set -- .
-                ;;
+	0)
+		set -- .
+		;;
 esac
-unset -v isort_v black_v
-if ${verbose}
-then
-	isort_v=--verbose
-	black_v=--verbose
-	set -x
-else
-	isort_v=
-	black_v=--quiet
-fi
-isort --profile=black ${isort_v} "$@" || exit
-black ${black_v} "$@" || exit
-#autopep8 --in-place --aggressive --aggressive --recursive "$@" || exit
+ruff ${verbose} check --fix "$@" || exit
+ruff ${verbose} format "$@" || exit
